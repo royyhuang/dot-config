cf69a0183fb518d61d20f4e1c9dbc648601a36c6
Roy Huang <huang.2928@osu.edu>
2022-10-22 21:11:07 -0500

    refrac(nvim): parse the nvim config from vimscript to lua

---
+++ nvim/init.lua
@@ -0,0 +1,162 @@
+-- General vim options
+vim.opt.encoding="utf-8"
+vim.opt.colorcolumn="80"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.showcmd = true
+vim.opt.laststatus = 3
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.background = "dark"
+vim.opt.backspace = "2"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.showtabline = 2
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.hidden = true
+vim.opt.updatetime = 100
+vim.opt.shortmess = "c"
+vim.cmd "syntax enable"
+vim.cmd "filetype indent on"
+
+-- general vim keybindings
+local keyset = vim.keymap.set
+keyset("n", "k", "(v:count == 0 ? 'gk' : 'k')", {expr = true})
+keyset("n", "j", "(v:count == 0 ? 'gj' : 'j')", {expr = true})
+keyset("i", "jj", "<ESC>")
+
+-- Packer packages
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  use 'marko-cerovac/material.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'nvim-lualine/lualine.nvim'
+  use 'alvarosevilla95/luatab.nvim'
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+  	  branch = 'master',
+	  run = ':TSUpdate'
+  }
+  use {
+    'neoclide/coc.nvim',
+    branch = 'release',
+  }
+  use 'christoomey/vim-tmux-navigator'
+  use 'voldikss/vim-floaterm'
+  use 'edkolev/tmuxline.vim'
+  use 'easymotion/vim-easymotion'
+  use 'airblade/vim-gitgutter'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- coc keybindings
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+keyset("n", "<C-e>", ":CocCommand explorer<cr>")
+keyset("n", "<space>rf", "<Plug>(coc-refractor)")
+keyset("n", "<space>a", ":CocAction<cr>")
+keyset("x", "<space>a", ":CocAction<cr>")
+keyset("i", "<TAB>",'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+
+-- Highlight the symbol and its references when holding the cursor.
+vim.api.nvim_create_augroup("CocGroup", {})
+vim.api.nvim_create_autocmd("CursorHold", {
+    group = "CocGroup",
+    command = "silent call CocActionAsync('highlight')",
+    desc = "Highlight symbol under cursor on CursorHold"
+})
+
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+	enable = true
+  },
+}
+
+require('material').setup({
+	lualine_style = 'stealth', -- the stealth style
+})
+vim.cmd "colorscheme material"
+
+require('lualine').setup {
+  options = {
+    theme = 'material',
+	section_separators = '',
+	component_separators = '',
+  }
+}
+
+-- tmuxline configs
+vim.g.tmuxline_powerline_separators = 0
+vim.g.tmuxline_theme = "vim_statusline_1"
+vim.cmd [[
+let g:tmuxline_preset = {
+  \ 'a': '[#S]',
+  \ 'win': '#I:#W#F',
+  \ 'cwin': '#I:#W#F',
+  \ 'z': '%H:%M %b-%d-%y',
+  \ 'options': {
+  \   'status-justify': 'left'}
+  \}
+]]
+
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    }
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+require('telescope').load_extension('fzf')
+-- telescope keybindings
+local builtin = require('telescope.builtin')
+keyset("n", "<space>ff", builtin.find_files, {})
+keyset("n", "<space>fj", builtin.live_grep, {})
+keyset("n", "<space>fb", builtin.buffers, {})
+keyset("n", "<space>fh", builtin.help_tags, {})
+
+require('luatab').setup {}
